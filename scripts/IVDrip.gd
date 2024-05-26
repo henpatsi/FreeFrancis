d41b668012a9ce17c7760093da9a00efe4047310
Henri Pätsi <patsihenri@gmail.com>
2024-05-26 14:10:39 +0300

    cleaner iv and jumping with iv

--- scripts/IVDrip.gd
+++ scripts/IVDrip.gd
@@ -1,23 +1,28 @@
 extends Node3D
 
-@onready var rigid_body_3d: RigidBody3D = $RigidBody3D
+@onready var character_body_3d: CharacterBody3D = $".."
+@onready var area_3d: Area3D = $Area3D
 
-var mouse_stop_timer: float = 0.0
-var mouse_stop_delay: float = 0.2
+var max_distance_from_player: float = 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	area_3d.monitoring = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if mouse_stop_timer <= 0:
-		rigid_body_3d.linear_velocity = Vector3.ZERO
-		rigid_body_3d.angular_velocity = Vector3.ZERO
-	else:
-		mouse_stop_timer -= delta
+	
+	var distance_vector: Vector3 = area_3d.global_position - character_body_3d.global_position
+	var distance_magnitude: float = abs(distance_vector.length())
+	if distance_magnitude > max_distance_from_player:
+		var move_back_distance_vector: Vector3 = distance_vector * ((distance_magnitude - max_distance_from_player) / distance_magnitude)
+		move_back_distance_vector.z = 0
+		area_3d.global_position -= move_back_distance_vector
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rigid_body_3d.apply_force(Vector3(event.relative.x, -event.relative.y, 0))
-		mouse_stop_timer = mouse_stop_delay
+		area_3d.position += Vector3(event.relative.x, -event.relative.y, 0) * 0.01
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("Collided with: ", body.name)
+	character_body_3d.velocity.y += 5
